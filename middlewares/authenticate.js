f7a763c03b6e1205fd2543d439fded7309bc2a76
Rammbest <igor-kutsenko@ukr.net>
2024-06-02 21:55:01 +0300

    add middleware authenticate

---
+++ middlewares/authenticate.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+import HttpError from "../helpers/HttpError";
+import ctrlWrapper from "../decorators/ctrlWrapper";
+import User from "../models/user-model";
+
+const { JWT_SECRET } = process.env;
+
+const authenticate = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer") {
+    throw HttpError(401, "Not authorized");
+  }
+
+  try {
+    const { id } = jwt.verify(token, JWT_SECRET);
+    const user = await User.findById(id);
+    if (!user || !user.token || user.token !== token) {
+      throw HttpError(401, "Not authorized");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    next(HttpError(401, "Not authorized"));
+  }
+};
